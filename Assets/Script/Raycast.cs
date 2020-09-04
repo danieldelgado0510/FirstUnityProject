65fbeba30785606f6ae579ceff1837fa97bda026
Jesse <jdsmithz42@gmail.com>
2020-09-04 12:59:26 -0400

    First submission

---
+++ Assets/Script/Raycast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Raycast : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //From Unity Tutorial
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    Debug.Log("This is a mouse!");
+                    text.text = "This is a mouse!";
+                }
+            }
+        }
+    }
+}
